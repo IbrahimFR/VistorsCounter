7e785069a5f71603bfa667e9127bdfb6f01a81b3
ibrahimfouad <ifouad@kepler51.com>
2022-12-03 23:26:07 +0100

    add file anonymous generator

---
+++ src/main/java/com/flightright/counter/utils/FileGenerator.java
@@ -0,0 +1,34 @@
+package com.flightright.counter.utils;
+
+import net.andreinc.mockneat.MockNeat;
+import net.andreinc.mockneat.types.enums.StringType;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.WRITE;
+import static net.andreinc.mockneat.types.enums.DomainSuffixType.POPULAR;
+
+public class FileGenerator {
+    public static void main(String[] args) {
+        generateCsvFile();
+    }
+
+   private static void generateCsvFile(){
+       MockNeat m = MockNeat.threadLocal();
+       final Path path = Paths.get("./src/main/resources/test2.csv");
+
+       m.fmt("#{email},#{phone},#{source}")
+               .param("email", m.emails())
+               .param("phone", m.strings().size(10).types(StringType.NUMBERS))
+               .param("source", m.urls().domains(POPULAR))
+               .list(10000000)
+               .consume(list -> {
+                   try { Files.write(path, list, CREATE, WRITE); }
+                   catch (IOException e) { e.printStackTrace(); }
+               });
+   }
+}
